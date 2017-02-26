61dff16c7cffec90d011d891caa570eadd1a7c7e
yuukuma12 <yuukuma12@yahoo.co.jp>
2017-02-26 19:11:34 +0900

    Add: ○×オブジェクトの作成

---
+++ Assets/Scripts/InputController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+    GameObject click_obj = GetClickObject();
+    if (click_obj != null) {
+      Debug.Log(click_obj.name);
+    }
+	}
+
+  /// クリックされたオブジェクトの取得
+  /// @retval == null : クリックされなかった / オブジェクトをクリックしなかった
+  GameObject GetClickObject() {
+    if (Input.GetMouseButtonDown(0)) {
+      Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+      RaycastHit hit = new RaycastHit();
+      if(Physics.Raycast(ray, out hit)) {
+        return hit.collider.gameObject;
+      }
+    }
+    return null;
+  }
+}
