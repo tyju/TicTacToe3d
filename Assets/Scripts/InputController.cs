6fa79178118fa53ec6c60e036a555f1b2916e7b2
mug_kuzz <yuukuma12@yahoo.co.jp>
2017-04-30 21:27:30 +0900

    Chg: 横方向へのフリック処理を追加

--- Assets/Scripts/InputController.cs
+++ Assets/Scripts/InputController.cs
@@ -3,11 +3,19 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class InputController : MonoBehaviour {
+  public  GameObject  obj;
+  public  int   FLICK_FLAME_MIN    = 50;
+  public  int   FLICK_DISTANCE_MIN = 10;
+  public  float FLICK_SPEED        = 10;
+  private float     flick_frame      ;
+  private Vector3   flick_pos_start  ;
+  private Quaternion rotate_pos       ;
 
   //===== Event Function =====//
 	void Start () {
-		
-	}
+    flick_frame = 0;
+    rotate_pos  = obj.transform.rotation;
+  }
 	void Update () {
     GameObject click_obj = GetClickObject();
     if (click_obj) {
@@ -15,13 +23,28 @@ public class InputController : MonoBehaviour {
         click_obj.GetComponent<GridSpace>().SetObject();
       }
     }
+
+    // フリック用カウント
+    {
+      if(Input.GetMouseButton(0)) {
+        if(flick_frame != 0) { flick_frame++; }
+      }
+      if(Input.GetMouseButtonDown(0)) {
+        flick_pos_start = Input.mousePosition;
+        flick_frame = 1;
+      }
+      if(Input.GetMouseButtonUp(0)) {
+        flick_frame = 0;
+      }
+    }
+    GetFlick();
 	}
 
   //===== Private Function =====//
   // クリックされたオブジェクトの取得
   // @retval == null : クリックされなかった / オブジェクトをクリックしなかった
   private GameObject GetClickObject() {
-    if (Input.GetMouseButtonDown(0)) {
+    if(Input.GetMouseButtonDown(0)) {
       Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
       RaycastHit hit = new RaycastHit();
       if(Physics.Raycast(ray, out hit)) {
@@ -29,5 +52,33 @@ public class InputController : MonoBehaviour {
       }
     }
     return null;
+  }
+
+  // フリック
+  private void GetFlick() {
+    if(flick_frame > FLICK_FLAME_MIN) {
+      Vector3 flick_pos_end = Input.mousePosition;
+      if(Vector3.Distance(flick_pos_start, flick_pos_end) > FLICK_DISTANCE_MIN ) {
+        // 左回転
+        if(flick_pos_start.x - flick_pos_end.x > FLICK_DISTANCE_MIN) {
+          SetFlickRotate(new Vector3(0, 90,0));
+        }
+        // 右回転
+        if(flick_pos_end.x - flick_pos_start.x > FLICK_DISTANCE_MIN) {
+          SetFlickRotate(new Vector3(0,-90,0));
+        }
+        // 上回転
+        if(flick_pos_end.y - flick_pos_start.y > FLICK_DISTANCE_MIN) {
+          //SetFlickRotate(new Vector3( 90,0,0));
+        }
+      }
+    }
+    obj.transform.rotation = Quaternion.Slerp(obj.transform.rotation, rotate_pos, FLICK_SPEED * Time.deltaTime);
+  }
+
+  private void SetFlickRotate(Vector3 rot) {
+    obj.transform.rotation = rotate_pos;
+    flick_frame = 0;
+    rotate_pos = obj.transform.rotation * Quaternion.Euler(rot);
   }
 }
