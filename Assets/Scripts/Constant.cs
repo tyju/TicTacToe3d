e0e9f21a635aa7bbcda6d4817dd4cf32d8fdc14b
yuukuma12 <yuukuma12@yahoo.co.jp>
2017-05-14 23:19:09 +0900

    Chg: タグをシンボル化

--- Assets/Scripts/Constant.cs
+++ Assets/Scripts/Constant.cs
@@ -3,11 +3,30 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-// 周回したclamp(ex: 8,10,24 -> 22)
-public class Constant{
-  public static float Orbit(float value, float min, float max) {
-    float ret = value - min;
-    if(ret < 0) { return max + ret; }
-    return ret % (max - min) + min;
+namespace ConstantNs {
+  // タグ
+  public enum TAG_CONV {
+    NONE,
+    MAIN_CAMERA,
+    GAME_CTRL,
+    GRID,
+  };
+
+  // 周回したclamp(ex: 8,10,24 -> 22)
+  public class Constant {
+    public static float Orbit(float value, float min, float max) {
+      float ret = value - min;
+      if (ret < 0) { return max + ret; }
+      return ret % (max - min) + min;
+    }
+
+    public static string GetTag(TAG_CONV e_tag) {
+      switch (e_tag) {
+        default: Debug.Assert(false, "Not Exist Tag :" + e_tag); return "";
+        case TAG_CONV.MAIN_CAMERA : return "MainCamera"     ;
+        case TAG_CONV.GAME_CTRL   : return "GameController" ;
+        case TAG_CONV.GRID        : return "Grid"           ;
+      }
+    }
   }
 }
--- Assets/Scripts/InputController.cs
+++ Assets/Scripts/InputController.cs
@@ -1,12 +1,14 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Cst = ConstantNs.Constant; using Tag = ConstantNs.TAG_CONV;
 
 public class InputController : MonoBehaviour {
-  public  GameObject  obj;
+  public  GameObject  GridsCObj;
   public  int   FLICK_FLAME_MIN    = 50;  // フリックとみなすフレーム数
   public  int   FLICK_DISTANCE_MIN = 10;  // フリックとみなすマウス差分位置
   public  float FLICK_SPEED        = 10;  // フリック時のオブジェクト回転速度
+
   private float     flick_frame        ;  // フリックカウント値(フレーム)
   private Vector3   flick_pos_start    ;  // フリック開始時点のマウス位置
   private Quaternion m_rotate_pos      ;  // フリック時のオブジェクト回転の目標角度
@@ -14,12 +16,12 @@ public class InputController : MonoBehaviour {
   //===== Event Function =====//
 	void Start () {
     flick_frame = 0;
-    m_rotate_pos  = obj.transform.rotation;
+    m_rotate_pos  = GridsCObj.transform.rotation;
   }
 	void Update () {
     GameObject click_obj = GetClickObject();
     if (click_obj) {
-      if(click_obj.tag == "Grid") {
+      if(click_obj.tag == Cst.GetTag(Tag.GRID)) {
         click_obj.GetComponent<GridSpace>().SetObject();
       }
     }
@@ -56,7 +58,7 @@ public class InputController : MonoBehaviour {
 
   // フリック
   private void GetFlick() {
-    obj.transform.rotation = Quaternion.Slerp(obj.transform.rotation, m_rotate_pos, FLICK_SPEED * Time.deltaTime);
+    GridsCObj.transform.rotation = Quaternion.Slerp(GridsCObj.transform.rotation, m_rotate_pos, FLICK_SPEED * Time.deltaTime);
     if(flick_frame > FLICK_FLAME_MIN) {
       Vector3 flick_pos_end = Input.mousePosition;
       if(Vector3.Distance(flick_pos_start, flick_pos_end) > FLICK_DISTANCE_MIN ) {
@@ -82,17 +84,17 @@ public class InputController : MonoBehaviour {
 
   // フリックによる回転の設定
   private void SetFlickRotate(Vector3 rot) {
-    obj.transform.rotation = m_rotate_pos;
-    Vector3 local_rot = obj.transform.worldToLocalMatrix.MultiplyVector(rot);
+    GridsCObj.transform.rotation = m_rotate_pos;
+    Vector3 local_rot = GridsCObj.transform.worldToLocalMatrix.MultiplyVector(rot);
     flick_frame = 0;
-    bool is_rotY = Mathf.Abs(Constant.Orbit(m_rotate_pos.eulerAngles.x, -180f, 180f)) > 85f
-      || Mathf.Abs(Constant.Orbit(m_rotate_pos.eulerAngles.z, -180f, 180f)) > 85f;
+    bool is_rotY = Mathf.Abs(Cst.Orbit(m_rotate_pos.eulerAngles.x, -180f, 180f)) > 85f
+      || Mathf.Abs(Cst.Orbit(m_rotate_pos.eulerAngles.z, -180f, 180f)) > 85f;
 
     Quaternion rotate_pos = m_rotate_pos * Quaternion.Euler(local_rot);
 
     // 上下90度回転している場合は戻る回転しか許可しない
-    if(is_rotY && (Mathf.Abs(Constant.Orbit(rotate_pos.eulerAngles.x, -180f, 180f)) > 85f
-      || Mathf.Abs(Constant.Orbit(rotate_pos.eulerAngles.z, -180f, 180f)) > 85f)) {
+    if(is_rotY && (Mathf.Abs(Cst.Orbit(rotate_pos.eulerAngles.x, -180f, 180f)) > 85f
+      || Mathf.Abs(Cst.Orbit(rotate_pos.eulerAngles.z, -180f, 180f)) > 85f)) {
       return;
     }
 
